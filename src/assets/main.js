b3ebce755b4936554f5c5802466a3a15bddd8e3d
Charles Peterson <nowledster@gmail.com>
2017-11-10 16:45:58 +0300

    Finished JQuery Project

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,4 +1,37 @@
 $(function() {
+   $.ajax({
+    url: "https://www.codeschool.com/users/nowled.json",
+    dataType: 'jsonp',
+    success: function(response){
+      addCourses(response.courses.completed);
+    }
+
+  });
+
+  function addCourses(courses){
+      var $badges = $("#badges");
+      courses.forEach(function(course){
+      var $course =  $('<div/>', {
+          'class': 'course'
+        }).appendTo($badges);
+
+        $('<h3/>',{
+          text: course.title
+        }).appendTo($course);
+
+        $('<img />', {
+            src: course.badge
+        }).appendTo($course);
+
+        $('<a />', {
+          'class': 'btn btn-primary',
+          target: '_blank',
+          href: course.url,
+          text: 'See Course'
+        }).appendTo($course);
+      })
+
+  }
 
   // your code will go here
 
